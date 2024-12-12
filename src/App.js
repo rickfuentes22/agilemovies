d67bc97113bd881d9dd81b55350feab3a906b067
rickfuentes22 <anibal.fe47@gmail.com>
2024-12-12 15:57:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Estrenos from './pages/Estrenos';
+import Home from './pages/Home';
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/estrenos" element={<Estrenos />} />
+        <Route path="/home" element={<Home />} />
+
       </Routes>
     </Router>
   );
